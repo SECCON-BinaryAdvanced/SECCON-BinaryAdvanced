0b22444834fc68e46f2764b5c87b70a5d024f0db
SECCON-BinaryAdvanced <seccon.binaryadvanced@gmail.com>
2026-02-28 11:33:30 +0900

    Delete session2/target directory

--- session2/target/miniimg/harness_libfuzzer.c
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <stddef.h>
-#include "miniimg.h"
-int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    MiniImg *img = miniimg_parse(data, size);
-    if (img) { miniimg_invert(img); miniimg_flip_h(img); miniimg_free(img); }
-    return 0;
-}
